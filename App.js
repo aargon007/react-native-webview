86ba3d10aacbbdec5d9350b42e03b8d35433582c
md muhaiminul <muhai4959@gmail.com>
2024-02-04 15:37:44 +0600

    added web view

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { WebView } from "react-native-webview";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<StatusBar barStyle={"default"} />
+			<WebView
+				style={styles.webview}
+				bounces={false}
+				source={{ uri: "https://wnw.app/" }}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+	},
+	webview: {
+		flex: 1,
+		marginTop: 30,
+	},
 });
